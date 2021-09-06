a67cdda7b6ab0018066b38b4aa7c555a2a091404
Awais Mansha <awaismansha1998@gmail.com>
2021-09-06 22:17:49 +0500

    working on lesson 3 Kotlin

--- app/src/main/java/com/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/androidtrivia/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.androidtrivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -16,6 +18,19 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
 
+        // 1 way
+//        binding.playButton.setOnClickListener { view:View ->
+//            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        // 2 way
+//        binding.playButton.setOnClickListener { view:View ->
+//            // kotlin extension function to reduce code structure
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        // 3 way
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 
